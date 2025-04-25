4a9bec04a604c4f4c9f809635d91d23eb929cc7d
NikolayDimitrov69 <ndllist@gmail.com>
2025-04-25 18:48:34 +0300

    Added some new constants

--- sfml-project2/Constants.h
+++ sfml-project2/Constants.h
@@ -1,13 +1,16 @@
 #pragma once
 
 // GAME constants
-constexpr float		GAME_PROGRESSION_PER_FRAME = 0.01f;
+constexpr float		GAME_PROGRESSION_PER_FRAME = 0.00f;
 constexpr float		GAME_SLOPE_SPAWN_TIMER = 225.f;
 constexpr float		GAME_ITEM_SLOPE_SPAWN_TIMER = 1000.f;
 constexpr float		GAME_INIT_SLOPE_FALL_SPEED = 1.2f;
 constexpr int		GAME_WINDOW_WIDTH = 1600;
 constexpr int		GAME_WINDOW_HEIGHT = 800;
 constexpr float     GAME_WINDOW_SCALE = 2;
+constexpr float		GAME_MAX_PROGRESS = 1000000000.f;
+constexpr float		GAME_ATTACK_OFFSET_ANGLE = 5.f;
+constexpr int		GAME_DEFAULT_ATTACK_COUNT = 1;
 
 // MENU constants
 constexpr float		MENU_PRESS_TO_CONTINUE_TIMER = 250.f;
