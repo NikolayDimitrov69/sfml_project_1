2ed9db6003f70f51955774e921aa36d0dbe8647d
NikolayDimitrov69 <ndllist@gmail.com>
2025-05-02 00:50:30 +0300

    Added enemy spawner controller and added to systemObj

--- sfml-project2/GameEnvironmentService.cpp
+++ sfml-project2/GameEnvironmentService.cpp
@@ -1,9 +1,12 @@
 #include "precompheaders.h"
 #include "GameEnvironmentService.h"
+#include "EnemySpawnerController.h"
 
 GameEnvironmentService::GameEnvironmentService()
 {
     m_SystemObject = new GameObject();
+    ObjectMutator(m_SystemObject)
+        .add<EnemySpawnerController>();
 }
 
 GameObject* GameEnvironmentService::GetSystemObject() const
